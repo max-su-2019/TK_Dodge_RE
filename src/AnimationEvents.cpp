e330ce99becfeb3bfb8ddf9127a4d90b0497e4a4
max-su-2019 <54713650+max-su-2019@users.noreply.github.com>
2021-09-14 15:57:38 +0800

    GodMode not costing stamina.

--- src/AnimationEvents.cpp
+++ src/AnimationEvents.cpp
@@ -10,10 +10,10 @@ namespace TKDodge
 
 		if (a_event->holder && a_event->holder->IsPlayerRef()) {
 			auto datahandler = DataHandler::GetSingleton();
-
 			if (a_event->tag == "TKDR_DodgeStart") {
 				logger::debug(FMT_STRING("TK Dodge Start, Time {}!"),clock());
-				RE::PlayerCharacter::GetSingleton()->As<RE::ActorValueOwner>()->RestoreActorValue(RE::ACTOR_VALUE_MODIFIER::kDamage, RE::ActorValue::kStamina, -datahandler->settings->dodgeStamina);
+				if (!RE::PlayerCharacter::IsGodMode())
+					RE::PlayerCharacter::GetSingleton()->As<RE::ActorValueOwner>()->RestoreActorValue(RE::ACTOR_VALUE_MODIFIER::kDamage, RE::ActorValue::kStamina, -datahandler->settings->dodgeStamina);
 			} else if (a_event->tag == "TKDR_IFrameEnd") {
 				logger::debug(FMT_STRING("Invulnerable Frame End!, Time {}"), clock());	
 			}
