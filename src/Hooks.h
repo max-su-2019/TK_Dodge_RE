e79f5dbfe86a3f2c1b039641c54129242886ddcb
max-su-2019 <54713650+max-su-2019@users.noreply.github.com>
2021-09-26 10:47:13 +0800

    Update Invulnerable to Magic

--- src/Hooks.h
+++ src/Hooks.h
@@ -8,11 +8,16 @@ namespace TKDodge
 		static void InstallHook()
 		{
 			SKSE::AllocTrampoline(1 << 4);
+			auto& trampoline = SKSE::GetTrampoline();
 
 			REL::Relocation<std::uintptr_t> ProcessHitFunc{ REL::ID(37673) };
-
-			auto& trampoline = SKSE::GetTrampoline();
 			_IsGhost = trampoline.write_call<5>(ProcessHitFunc.address() + 0x45, IsGhost);
+
+			REL::Relocation<std::uintptr_t> NPCInvulnerableBase{ REL::ID(36715) };
+			_IsGhost = trampoline.write_call<5>(NPCInvulnerableBase.address() + 0x2A, IsGhost);
+
+			REL::Relocation<std::uintptr_t> PCInvulnerableBase{ REL::ID(39428) };
+			_IsGhost = trampoline.write_call<5>(PCInvulnerableBase.address() + 0x14, IsGhost);
 		}
 
 	private:
